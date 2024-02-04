909d77bc3e685c9327497f897d0a07a3a74f75c7
Pixailz <Pixailz1@gmail.com>
2024-02-04 20:55:40 +0100

    working base

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-if [ -d "/var/lib/mysql/mysql" ]; then
-    # ALREADY exists
-    echo "Database already exists"
+if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+	# ALREADY exists
+	echo "Database already exists"
 else
-    # NOT INITIALISED
+	# NOT INITIALISED
+	mysql_install_db --user="mysql" --ldata=/var/lib/mysql
 
-    mysql_install_db --user="mysql" --ldata=/var/lib/mysql
+	<<-EOSTRING cat > /tmp/grant.sql
+		CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+		USE ${MYSQL_DATABASE};
+		FLUSH PRIVILEGES;
+		GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' identified by '${MYSQL_PASS}' WITH GRANT OPTION ;
+		SET PASSWORD FOR '${MYSQL_USER}'@'%'=PASSWORD('${MYSQL_PASS}') ;
+		DROP DATABASE IF EXISTS test ;
+		FLUSH PRIVILEGES ;
+	EOSTRING
 
-    <<EOSTRING cat > /tmp/grant.sql
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE ${MYSQL_DATABASE};
-FLUSH PRIVILEGES;
-GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' identified by '${MYSQL_PASS}' WITH GRANT OPTION ;
-SET PASSWORD FOR '${MYSQL_USER}'@'%'=PASSWORD('${MYSQL_PASS}') ;
-DROP DATABASE IF EXISTS test ;
-FLUSH PRIVILEGES ;
-EOSTRING
-
-    /usr/sbin/mysqld --bootstrap --user=mysql --verbose=0 --skip-name-resolve --skip-networking=0 < /tmp/grant.sql
-    rm /tmp/grant.sql
+	/usr/sbin/mysqld --bootstrap --user=mysql --verbose=0 --skip-name-resolve --skip-networking=0 < /tmp/grant.sql
+	rm /tmp/grant.sql
 fi
 
 [ ! -d /run/mysqld ] && mkdir -p /run/mysqld
@@ -31,7 +30,7 @@ printf "MariaDB launched on port: \x1b[38:5:208m3306\x1b[0m\n"
 echo "Mariadb OK"
 
 if [ $# -gt 0 ]; then
-    exec "$@"
+	exec "$@"
 else
-    /usr/sbin/mysqld --console --user=mysql --verbose=0 --skip-name-resolve --skip-networking=0
+	/usr/sbin/mysqld --console --user=mysql --verbose=0 --skip-name-resolve --skip-networking=0 --bind-address=0.0.0.0
 fi
--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "Wordpress OK."
-chmod +x /tmp/wp-cli-2.9.0.phar
-/tmp/wp-cli-2.9.0.phar
+WP="wp --allow-root"
+
+if [ -f "/bin/wp" ]; then
+	echo "Wordpress OK."
+else
+	mv /tmp/wp /bin/wp && chmod +x /bin/wp
+
+	cd /var/www/html
+	${WP} core download
+	${WP} config create \
+		--dbname="${MYSQL_DATABASE}" \
+		--dbuser="${MYSQL_USER}" \
+		--dbpass="${MYSQL_PASS}" \
+		--dbhost=mariadb
+fi
